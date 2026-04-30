c2995fd5c5b87852aadffd58bf94fc2fedeaf3ee
Manuel Franco Jiménez Mendoza <142540969+ManuelMendoza0206@users.noreply.github.com>
2026-04-30 02:55:05 -0400

    Update API URL in environment configurationaaa

--- zoo-frontend/src/environment/environment.ts
+++ zoo-frontend/src/environment/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //marca: "ZooConnect",
   marca: "ZooConnect",
-  apiUrl: "https://zoo-connect-completo.onrender.com",
+  apiUrl: "https://zoo-connect-completo.onrender.com/zooconnect",
   //apiUrl: "https://zoo-connect-backend2.onrender.com/zooconnect",
   // ReCAPTCHA v2 Site Key (visible widget) - Obtener en https://www.google.com/recaptcha/admin
   recaptchaSiteKey: "6LemotEsAAAAAAPdcpfXaAdb2PfK93wKdGZownBh",
